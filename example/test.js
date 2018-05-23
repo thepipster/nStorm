777164511c18e9afec49d6e40c8104315b6d74f2
thepipster <mike@arsenicsoup.com>
2018-05-23 16:16:56 -0400

    initial branch creation

--- example/test.js
+++ example/test.js
@@ -1,69 +1,52 @@
-var Logger = require('arsenic-logger');
-var nStorm = require('../index.js');
+var Logger = require('../index.js').Logger
+var nStorm = require('../index.js').nStorm
+var BaseBolt = require('../index.js').BaseBolt
 
-// Spout and Bolt implementation
-var coinTossSpout = new CoinSpout();
-var headsBolt = new HeadsBolt();
-var tailsBolt = new TailsBolt();
-var resultsBolt = new ResultsBolt();
-
-var cloud = new nStorm({useCluster:false, debug:false});
-
-// Setting up topology using the topology builder
-cloud.addBlock("coindTossSpout", coinTossSpout);
-cloud.addBlock("tailsBolt", tailsBolt, 1).input("coindTossSpout", {coin: "tails"});
-cloud.addBlock("headsBolt", headsBolt, 1).input("coindTossSpout", {coin: "heads"});
-cloud.addBlock("resultsBolt", resultsBolt, 1).input("tailsBolt").input("headsBolt");
-
-// Setup cluster, and run topology...
-cloud.start();
-
+/**
+ * Returns a random integer between min and max
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 /**
  * Demo spout that generates random data
  */
-function CoinSpout() {
+class CoinSpout extends BaseBolt {
 
-    var test = 56;
-    this.temp = 47;
-
-    this.start = function(context) {
+    async start() {
 
         Logger.info("Starting...");
-        sendData();
 
-        function sendData(){
-            
-            for (var i=0; i<10; i++){
+        for (var i=0; i<10; i++){
 
-                var test = getRandomInt(0,100);
+            var test = getRandomInt(0,100);
 
-                var toss = 'tails';
-                if (test < 50){
-                    toss = 'heads';
-                }
-
-                var row = {coin: toss, time: Date.now()}
-
-                Logger.debug("Emitting", row);
-
-                context.emit(row);
+            var toss = 'tails';
+            if (test < 50){
+                toss = 'heads';
             }
 
-            setTimeout(function(){
-                sendData();
-            }, 1000);
+            var row = {coin: toss, time: Date.now()}
 
+            Logger.debug("Emitting", row);
+
+            this.emit(row);
+
+            await this.delay(1000)
         }
+
+        return
 
     }
 
 
 }
 
-function HeadsBolt() {
+class HeadsBolt extends BaseBolt {
 
-    this.process = function(message, context) {
+    process(message, context) {
 
         Logger.info("Heads ", message.coin);
 
@@ -77,9 +60,9 @@ function HeadsBolt() {
 
 }
 
-function TailsBolt() {
+class TailsBolt extends BaseBolt {
 
-    this.process = function(message, context) {
+    process(message, context) {
 
         Logger.info("Tails ", message.coin);
 
@@ -92,10 +75,9 @@ function TailsBolt() {
 
 }
 
+class ResultsBolt extends BaseBolt {
 
-function ResultsBolt() {
-
-    this.process = function(message, context) {
+    process(message, context) {
 
   //      Logger.info("Results Message ", message.coin);
 
@@ -116,10 +98,22 @@ function ResultsBolt() {
 
 }
 
-/**
- * Returns a random integer between min and max
- * Using Math.round() will give you a non-uniform distribution!
- */
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+
+
+// Spout and Bolt implementation
+var coinTossSpout = new CoinSpout();
+var headsBolt = new HeadsBolt();
+var tailsBolt = new TailsBolt();
+var resultsBolt = new ResultsBolt();
+
+var cloud = new nStorm({useCluster:false, debug:false});
+
+// Setting up topology using the topology builder
+cloud.addBlock("coindTossSpout", coinTossSpout);
+//cloud.addBlock("tailsBolt", tailsBolt, 1).input("coindTossSpout", {coin: "tails"});
+//cloud.addBlock("headsBolt", headsBolt, 1).input("coindTossSpout", {coin: "heads"});
+//cloud.addBlock("resultsBolt", resultsBolt, 1).input("tailsBolt").input("headsBolt");
+
+// Setup cluster, and run topology...
+cloud.start();
+
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-module.exports = require('./lib/nStorm.js');
+
+exports.nStorm = require('./lib/nStorm.js');
+exports.BaseBolt = require('./lib/BaseBolt.js');
+exports.Logger = require('./lib/Logger.js');
 
 
